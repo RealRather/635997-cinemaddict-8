032aecd54bfd122fabe574584d86bc1903b56eed
RealRather <36735076+RealRather@users.noreply.github.com>
2019-03-03 00:05:40 +0300

    Adds filter template

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import makeCard from './make-card.js';
+import makeFilter from './make-filter.js';
 
 const COUNT_CARDS = 7;
 const MIN_RANDOM_VALUE = 1;
-const MAX_RANDOM_VALUE = 10;
+const MAX_RANDOM_VALUE = 20;
 
 const filters = {
   all: `All movies`,
@@ -12,6 +13,7 @@ const filters = {
   stats: `Stats`
 };
 
+const mainNavBlock = document.querySelector(`.main-navigation`);
 const filmsCardsBlock = document.querySelector(`.films-list__container`);
 
 const getRandomFixValue = ((minValue, maxValue) => Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue);
@@ -25,7 +27,43 @@ const renderCards = ((countCards, container) => {
     );
 });
 
+const createFiltersElements = ((filtersObj) => {
+  const filtersKeys = Object.keys(filtersObj);
+  return filtersKeys
+    .map((key) => {
+      let count = getRandomFixValue(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE);
+      let state;
+      switch (key) {
+        case `all`:
+          state = `active`;
+          count = ``;
+          break;
+        case `stats`:
+          state = `additional`;
+          count = ``;
+          break;
+      }
+      return makeFilter(key, filters[key], state, count);
+    })
+    .join(``);
+});
 
 
+const renderFilters = ((container, filtersObj) => {
+  container.innerHTML = (createFiltersElements(filtersObj));
+  container.addEventListener(`click`, (evt) => onFilterLabelClickHandler(evt), true);
+});
+
+const onFilterLabelClickHandler = ((evt) => {
+  evt.stopPropagation();
+  if (evt.target.getAttribute(`href`) === `#stats`) {
+    return;
+  }
+  renderCards(
+      getRandomFixValue(1, COUNT_CARDS),
+      filmsCardsBlock
+  );
+});
 
 renderCards(COUNT_CARDS, filmsCardsBlock);
+renderFilters(mainNavBlock, filters);
