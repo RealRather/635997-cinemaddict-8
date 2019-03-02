a5cfd2e466b1c7b5d0abe5c973de201abbc353d5
RealRather <36735076+RealRather@users.noreply.github.com>
2019-03-02 15:44:13 +0300

    Adds filters obj

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import makeCard from './make-card.js';
 const COUNT_CARDS = 7;
 const MIN_RANDOM_VALUE = 1;
 const MAX_RANDOM_VALUE = 10;
-debugger;
+
+const filters = {
+  all: `All movies`,
+  watchlist: `Watchlist`,
+  history: `History`,
+  favorites: `Favorites`,
+  stats: `Stats`
+};
+
 const filmsCardsBlock = document.querySelector(`.films-list__container`);
 
 const getRandomFixValue = ((minValue, maxValue) => Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue);
@@ -17,5 +25,7 @@ const renderCards = ((countCards, container) => {
     );
 });
 
+
+
+
 renderCards(COUNT_CARDS, filmsCardsBlock);
-debugger;
